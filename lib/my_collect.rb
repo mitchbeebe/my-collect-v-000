37e4bee6c0a90220f5c2a57b98f8feadaba1e656
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-09-04 01:26:11 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,7 @@ def my_collect(array)
   i = 0
   collection = []
   while i < array.size
-    collection << array[i]
-    yield array[i]
+    collection << yield(array[i])
     i += 1
   end
   collection
